d3a75429648706a9962c404c7f8ab9503146b9ca
Bharadwaj <bharadwaj.gk007@gmail.com>
2017-09-19 08:52:39 +0530

    documenting code

--- app/js/controllers/AddNewCusomterController.js
+++ app/js/controllers/AddNewCusomterController.js
@@ -2,12 +2,13 @@
 customerOverviewApp.controller('AddNewCusomterController',
  function AddNewCusomterController($scope, CustomerServices, $location) {
      var vm = this;
+     /* Log the navigation when user enters add customer controller */
      var logNavigation = CustomerServices.logNavigation({
             'page': 'Add New',
             'timestamp': new moment().format("YYYY-MM-DD HH:mm:ss")
         });
      this.successMessage = "";
-
+    /* Saving customer using HTML5 local storage*/
      vm.saveCustomer = function() {     
          var customers = (localStorage.getItem("customers") !== null) ? JSON.parse(localStorage.getItem("customers")) : [];    
          vm.customer.id = customers.length + 1;       
@@ -16,7 +17,7 @@ customerOverviewApp.controller('AddNewCusomterController',
          this.successMessage = "Successfully Saved";
          $location.url('/overview');
      };
-     
+     /* Navigate to cancel */
      vm.cancel = function() {
          $location.url('/overview');
      };
